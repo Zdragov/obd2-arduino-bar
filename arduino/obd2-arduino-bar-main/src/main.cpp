c632437ed3a6dc1794aad81599b33c94c733fb2a
mahasuchon <turbococo@gmail.com>
2026-09-21 10:32:48 +1200

    Added emulation modes

--- arduino/obd2-arduino-bar-main/src/main.cpp
+++ arduino/obd2-arduino-bar-main/src/main.cpp
@@ -11,12 +11,16 @@ CRGB leds[LED_COUNT];
 
 int rpmEmu = 2000;
 int kphEmu = 80;
-int throttleEmu = 0.3; //min 0, max 1
+int throttleEmu = 20; //0 to 100
+int emuCmd = 0;
+
+int emuCruiseAccelerating = 1;
 
 
 
 int rpmRead = rpmEmu;
 int kphRead = kphEmu;
+
 int rpmMap;
 int kphMap;
 int throttleRead = throttleEmu;
@@ -65,11 +69,11 @@ void setup() {
 
 void loop() { 
 
-  //mock and test data
+  //mock and test data, and serial commands
   rpmRead = rpmEmu;
   kphRead = kphEmu;
 
-
+/*
   if (rpmEmu < 7000) {
     rpmEmu = rpmEmu + 100;
   } else {
@@ -82,7 +86,100 @@ void loop() {
     kphEmu = 0;
   }
 
-  
+*/
+
+
+
+
+
+  if (Serial.available() != 0) {
+    char receivedCmd = Serial.read();
+
+    switch (receivedCmd) {
+      case 'a':
+        emuCmd = 1;
+        Serial.println("Slowly Accelerating...");
+        kphEmu = 0;
+        rpmEmu = 2000;
+        break;
+      case 'b':
+        emuCmd = 2;
+        Serial.println("Braking...");
+        kphEmu = 100;
+        rpmEmu = 2000;
+        throttleEmu = 1;
+        break;
+      case 'c':
+        emuCmd = 3;
+        Serial.println("Cruising (95-105kph)...");
+        kphEmu = 100;
+        rpmEmu = 2250;
+        throttleEmu = 20;
+        break;
+      case 'd':
+        emuCmd = 4;
+        Serial.println("Quickly Accelerating...");
+        kphEmu = 0;
+        rpmEmu = 2000;
+        break;
+      default:
+        break;
+    }
+
+  }
+
+  if (emuCmd == 1) { // Emulate casual acceleration
+    
+    if (kphEmu < 100) {
+      rpmEmu = rpmEmu + 100;
+      kphEmu = kphEmu + 2;
+      if (rpmEmu > 3000) {
+        rpmEmu = 2000;
+      }
+      throttleEmu = 50;
+    } else {
+      kphEmu = 100;
+      rpmEmu =  2250;
+      throttleEmu = 20;
+    } 
+  } else if (emuCmd == 2) { // Emulate braking from 100kph
+    if (kphEmu > 0) {
+      rpmEmu = 1750;
+      kphEmu = kphEmu - 2;
+    }
+  } else if (emuCmd == 3) { // Emulate speed bouncing from 95kph to 105kph
+    if (emuCruiseAccelerating == 1) {
+      kphEmu = kphEmu + 1;
+      rpmEmu = rpmEmu + 50;
+      throttleEmu = 23;
+      if (kphEmu > 105) {
+        emuCruiseAccelerating = 0;
+      }
+    } else if (emuCruiseAccelerating == 0) {
+      kphEmu = kphEmu - 1;
+      rpmEmu = rpmEmu - 50;
+      throttleEmu = 17;
+      if (kphEmu < 95) {
+        emuCruiseAccelerating = 1;
+      }
+    }
+  } else if (emuCmd == 4) { // Emulate hard acceleration
+    if (kphEmu < 100) {
+      rpmEmu = rpmEmu + 500;
+      kphEmu = kphEmu + 5;
+      if (rpmEmu > 7000) {
+        rpmEmu = 4000;
+      }
+      throttleEmu = 90;
+    } else {
+      kphEmu = 100;
+      rpmEmu =  2250;
+      throttleEmu = 20;
+    } 
+  }
+
+
+
   //serial print
 
 
@@ -92,6 +189,8 @@ void loop() {
   Serial.print(kphRead);
   Serial.println("kph");
   Serial.println("");
+
+
 
   //code
 
