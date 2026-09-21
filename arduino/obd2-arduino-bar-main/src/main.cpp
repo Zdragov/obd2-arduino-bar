8c19f9a24c6ad82c4fc17c4f796eec4be656530a
mahasuchon <turbococo@gmail.com>
2026-09-21 20:04:10 +1200

    Changed emu else-if chain into switch

--- arduino/obd2-arduino-bar-main/src/main.cpp
+++ arduino/obd2-arduino-bar-main/src/main.cpp
@@ -1,32 +1,30 @@
 #include <Arduino.h>
-//#include <Adafruit_NeoPixel.h>
+// #include <Adafruit_NeoPixel.h>
 #include <FastLED.h>
 
 #define LED_PIN 9
 #define LED_COUNT 60
-#define LED_TYPE WS2812B //i think this is it?
+#define LED_TYPE WS2812B // i think this is it?
 #define COLOUR_ORDER GRB
 
 CRGB leds[LED_COUNT];
 
 int rpmEmu = 2000;
 int kphEmu = 80;
-int throttleEmu = 20; //0 to 100
+int throttleEmu = 20; // 0 to 100
 int engineTempEmu = 25;
 int emuCmd = 0;
 int displayCmd = 0;
 
 int emuCruiseAccelerating = 1;
 
-
-
-
 int rpmRead = rpmEmu;
 int kphRead = kphEmu;
 int throttleRead = throttleEmu;
 int engineTempRead = engineTempEmu;
 
-int emuInfoSet(int cmdSent,int spd,int rpm,int throttle,int engineTemp) {
+int emuInfoSet(int cmdSent, int spd, int rpm, int throttle, int engineTemp)
+{
   emuCmd = cmdSent;
   kphEmu = 100;
   rpmEmu = 2250;
@@ -38,18 +36,12 @@ int rpmMap;
 int kphMap;
 int engineTempMap;
 
-
-
 int lightSensor = 50;
 
+// Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-
-
-//Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-
-
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
 
   Serial.begin(38400);
@@ -61,11 +53,9 @@ void setup() {
   FastLED.show();
   delay(333);
 
-
   fill_solid(leds, LED_COUNT, CRGB::Blue);
   FastLED.show();
   delay(333);
-
 
   fill_solid(leds, LED_COUNT, CRGB::Green);
   FastLED.show();
@@ -75,228 +65,254 @@ void setup() {
   FastLED.show();
   delay(333);
 
-
-  //int rpmDisplay(
-    //display rpm
+  // int rpmDisplay(
+  // display rpm
   //)
-
 }
 
-void loop() { 
+void loop()
+{
 
-  //mock and test data, and serial commands
+  // mock and test data, and serial commands
   rpmRead = rpmEmu;
   kphRead = kphEmu;
 
-/*
-  if (rpmEmu < 7000) {
-    rpmEmu = rpmEmu + 100;
-  } else {
-    rpmEmu = 2000;
-  }
-  
-  if (kphEmu < 100) {
-    kphEmu = kphEmu + 2;
-  } else {
-    kphEmu = 0;
-  }
+  kphMap = constrain(map(kphRead, 0, 120, 0, LED_COUNT - 1), 0, LED_COUNT - 1);
+  rpmMap = constrain(map(rpmRead, 0, 7000, 0, LED_COUNT - 1), 0, LED_COUNT - 1);
+  engineTempMap = constrain(map(engineTempRead, 0, 120, 0, LED_COUNT - 1), 0, LED_COUNT - 1);
 
-*/
-
-
-
-
-
-
-  if (Serial.available() != 0) {
-    char receivedCmd = Serial.read();
-
-    switch (receivedCmd) {
-      case 'a':
-        emuCmd = 1;
-        Serial.println("Slowly Accelerating...");
-        kphEmu = 0;
-        rpmEmu = 2000;
-        engineTempEmu = 90;
-        break;
-      case 'b':
-        emuCmd = 2;
-        Serial.println("Braking...");
-        kphEmu = 100;
-        rpmEmu = 2000;
-        throttleEmu = 1;
-        engineTempEmu = 90;
-        break;
-      case 'c':
-        emuCmd = 3;
-        Serial.println("Cruising (95-105kph)...");
-        kphEmu = 100;
-        rpmEmu = 2250;
-        throttleEmu = 20;
-        engineTempEmu = 90;
-        break;
-      case 'd':
-        emuCmd = 4;
-        Serial.println("Quickly Accelerating...");
-        kphEmu = 0;
-        rpmEmu = 2000;
-        engineTempEmu = 90;
-        break;
-      case 'e':
-        emuCmd = 5;
-        Serial.println("Cold starting...");
-        kphEmu = 0;
-        rpmEmu = 2250;
-        throttleEmu = 5;
-        engineTempEmu = 25;
-        break;
-        
-        
-
-
-
-
-      case '1':
-        displayCmd = 1;
-        Serial.println("Showing RPM");
-        break;
-      case '2':
-        displayCmd = 2;
-        Serial.println("Showing KPH");
-        break;
-      case '3':
-        displayCmd = 3;
-        Serial.println("Showing Temp");
-        break;
-      default:
-        break;
+  /*
+    if (rpmEmu < 7000) {
+      rpmEmu = rpmEmu + 100;
+    } else {
+      rpmEmu = 2000;
     }
 
+    if (kphEmu < 100) {
+      kphEmu = kphEmu + 2;
+    } else {
+      kphEmu = 0;
+    }
+
+  */
+
+  if (Serial.available() != 0)
+  {
+    char receivedCmd = Serial.read();
+
+    switch (receivedCmd)
+    {
+    case 'a':
+      emuCmd = 1;
+      Serial.println("Slowly Accelerating...");
+      kphEmu = 0;
+      rpmEmu = 2000;
+      engineTempEmu = 90;
+      break;
+    case 'b':
+      emuCmd = 2;
+      Serial.println("Braking...");
+      kphEmu = 100;
+      rpmEmu = 2000;
+      throttleEmu = 1;
+      engineTempEmu = 90;
+      break;
+    case 'c':
+      emuCmd = 3;
+      Serial.println("Cruising (95-105kph)...");
+      kphEmu = 100;
+      rpmEmu = 2250;
+      throttleEmu = 20;
+      engineTempEmu = 90;
+      break;
+    case 'd':
+      emuCmd = 4;
+      Serial.println("Quickly Accelerating...");
+      kphEmu = 0;
+      rpmEmu = 2000;
+      engineTempEmu = 90;
+      break;
+    case 'e':
+      emuCmd = 5;
+      Serial.println("Cold starting...");
+      kphEmu = 0;
+      rpmEmu = 2650;
+      throttleEmu = 5;
+      engineTempEmu = 25;
+      break;
+
+    case '1':
+      displayCmd = 1;
+      Serial.println("Showing RPM");
+      break;
+    case '2':
+      displayCmd = 2;
+      Serial.println("Showing KPH");
+      break;
+    case '3':
+      displayCmd = 3;
+      Serial.println("Showing Temp");
+      break;
+    default:
+      break;
+    }
   }
 
-  if (emuCmd == 1) { // Emulate casual acceleration          ALSO, convert this entire thing to switch case at some point
-    
-    if (kphEmu < 100) {
+switch (emuCmd) {
+	case 1: 
+	if (kphEmu < 100)
+    {
       rpmEmu = rpmEmu + 100;
       kphEmu = kphEmu + 2;
-      if (rpmEmu > 3000) {
+      if (rpmEmu > 3000)
+      {
         rpmEmu = 2000;
       }
       throttleEmu = 50;
-    } else {
+    }
+    else
+    {
       kphEmu = 100;
-      rpmEmu =  2250;
+      rpmEmu = 2250;
       throttleEmu = 20;
-    } 
-  } else if (emuCmd == 2) { // Emulate braking from 100kph
-    if (kphEmu > 0) {
+    }
+    break:
+    
+    case 2:
+    // Emulate braking from 100kph
+    if (kphEmu > 0)
+    {
       rpmEmu = 1750;
       kphEmu = kphEmu - 2;
     }
-  } else if (emuCmd == 3) { // Emulate speed bouncing from 95kph to 105kph
-    if (emuCruiseAccelerating == 1) {
+    break:
+  
+  	case 3:
+  	// Emulate speed bouncing from 95kph to 105kph
+    if (emuCruiseAccelerating == 1)
+    {
       kphEmu = kphEmu + 1;
       rpmEmu = rpmEmu + 50;
       throttleEmu = 23;
-      if (kphEmu > 105) {
+      if (kphEmu > 105)
+      {
         emuCruiseAccelerating = 0;
       }
-    } else if (emuCruiseAccelerating == 0) {
+    }
+    else if (emuCruiseAccelerating == 0)
+    {
       kphEmu = kphEmu - 1;
       rpmEmu = rpmEmu - 50;
       throttleEmu = 17;
-      if (kphEmu < 95) {
+      if (kphEmu < 95)
+      {
         emuCruiseAccelerating = 1;
       }
     }
-  } else if (emuCmd == 4) { // Emulate hard acceleration
-    if (kphEmu < 100) {
+    break;
+    
+    case 4:
+    // Emulate hard acceleration
+    if (kphEmu < 100)
+    {
       rpmEmu = rpmEmu + 500;
       kphEmu = kphEmu + 5;
-      if (rpmEmu > 7000) {
+      if (rpmEmu > 7000)
+      {
         rpmEmu = 4000;
       }
       throttleEmu = 90;
-    } else {
+    }
+    else
+    {
       kphEmu = 100;
-      rpmEmu =  2250;
+      rpmEmu = 2250;
       throttleEmu = 20;
-    } 
-  } else if (emuCmd == 5) {
-    if (engineTempEmu < 90) {
+    }
+    break;
+    
+    case 5:
+        if (engineTempEmu < 90)
+    {
       engineTempEmu++;
-      rpmEmu = rpmEmu - 10
-    //do X
-  }
+      rpmEmu = rpmEmu - 10;
+      // do X
+    }
+}
 
-  switch (displayCmd) {
-    case 1:
+
+  switch (displayCmd)
+  {
+  case 1:
     Serial.print("abcd");
     FastLED.clear();
-    //RPM display start
+    // RPM display start
 
-    //if RPM is increasing...
-    fadeToBlackBy(leds, LED_COUNT,70);
-    //else if RPM is decreasing...
-    //fadeToBlackBy(leds, LED_COUNT,150);
+    // if RPM is increasing...
+    fadeToBlackBy(leds, LED_COUNT, 70);
+    // else if RPM is decreasing...
+    // fadeToBlackBy(leds, LED_COUNT,150);
 
     leds[rpmMap] = CHSV(0, 255, 255);
-      
+
     FastLED.show();
 
-    //RPM display end
+    // RPM display end
     break;
 
-    case 2:
-    //Speedometer display Start
+  case 2:
+    // Speedometer display Start
 
     FastLED.clear();
 
-    fill_solid(&leds[0], kphMap, CRGB(120,120,120));
+    fill_solid(&leds[0], kphMap, CRGB(120, 120, 120));
 
-    leds[10] = CRGB::Yellow; //20kph
-    leds[20] = CRGB::Yellow; //40kph
-    leds[25] = CRGB::Green; //50kph
-    leds[30] = CRGB::Yellow; //60kph
-    leds[40] = CRGB::Yellow; //80kph
-    leds[50] = CRGB::Green; //100kph
+    leds[10] = CRGB::Yellow; // 20kph
+    leds[20] = CRGB::Yellow; // 40kph
+    leds[25] = CRGB::Green;  // 50kph
+    leds[30] = CRGB::Yellow; // 60kph
+    leds[40] = CRGB::Yellow; // 80kph
+    leds[50] = CRGB::Green;  // 100kph
 
     FastLED.show();
 
-    //Speedometer display End
+    // Speedometer display End
     break;
 
-    case 3:
+  case 3:
 
-    //Water Temp display Start (Only this is displayed on startup. Disappears when speed goes above 10kph)
-    
-    //meter goes from 20 to 120
-    //20-40c = too cold
-    //40-70c = cold but OK
-    //70-100c = OK
-    //100-120c = too hot
+    FastLED.clear();
 
-    //if engine temp is detected,
-    fill_solid(&leds[0], 12, CHSV(180,100,100));
-    fill_solid(&leds[12], 18, CHSV(250,100,100));
-    fill_solid(&leds[45], 15, CHSV(0,100,100));
+    // Water Temp display Start (Only this is displayed on startup. Disappears when speed goes above 10kph)
 
-    leds[engineTempMap] = CHSV(0,0,100);
+    // meter goes from 20 to 120
+    // 20-40c = too cold
+    // 40-70c = cold but OK
+    // 70-100c = OK
+    // 100-120c = too hot
 
-    if (engineTempRead > 105){
-      //flash red zone
+    // if engine temp is detected,
+    fill_solid(&leds[0], 12, CHSV(180, 255, 100));
+    fill_solid(&leds[12], 18, CHSV(250, 255, 100));
+    fill_solid(&leds[45], 15, CHSV(0, 255, 100));
+
+    leds[engineTempMap] = CHSV(0, 0, 100);
+
+    if (engineTempRead > 105)
+    {
+      // flash red zone
     }
 
+    FastLED.show();
+
     break;
 
-    //Water Temp display End
-
+    // Water Temp display End
   }
 
-
-
-  //serial print
-
+  //compile every layer into one
+  
+  // serial print
 
   Serial.print(rpmRead);
   Serial.println("rpm");
@@ -305,22 +321,10 @@ void loop() {
   Serial.println("kph");
   Serial.println("");
 
-
-
-  //code
-
-  
-
-  
-
-
-
+  // code
 
   delay(300);
 }
-
-
-
 
 /*
 
@@ -347,3 +351,5 @@ Use HSV color space for more intuitive color selection:
 fill_solid(leds, NUM_LEDS, CHSV(160, 255, 255));
 
 */
+
+// ctrl shift i to auto indent
